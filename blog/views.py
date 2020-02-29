deec43b8739d3b1d4bb2f3036403819cd693265e
Dillon Hanni <dillonhanni@Dillons-MacBook.local>
2020-02-29 11:51:44 -0600

    Added database support

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Dillon Hanni',
-        'title': 'Blog Post 1',
-        'content': 'First post content, written in Python!',
-        'date_posted': 'February 29, 2020'
-    },
-    {
-        'author': 'Richard Hanni',
-        'title': 'Blog Post Number 2',
-        'content': 'Second post content, this is awesome',
-        'date_posted': 'February 28, 2020'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
